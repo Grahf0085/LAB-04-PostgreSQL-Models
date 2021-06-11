bf20e256d76927c3260e0c1e9b3a514683b2c018
Tucker <tuckerhoog@tutanota.com>
2021-06-10 20:55:20 -0400

    post route test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,22 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('blattodea routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a blattodea via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/blattodeas')
+      .send({ name: 'buddy', habitat: 'fresh water wetland', length: 1 });
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'buddy',
+      habitat: 'fresh water wetland',
+      length: 1,
+    }); 
+  });
 });
+
--- data/setup.js
+++ data/setup.js
@@ -2,6 +2,7 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export default (pool) => {
+
   return fs
     .readFile(
       `${path.dirname(new URL(import.meta.url).pathname)}/../sql/setup.sql`,
@@ -11,3 +12,7 @@ export default (pool) => {
     )
     .then((sql) => pool.query(sql));
 };
+
+// module.exports = (pool) => {
+//   return fs.readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
+//     .then(sql => pool.query(sql));
